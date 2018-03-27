5b25e14b3febd37229e8624698db53f9f0086302
Joakim Timm Kromann-Rasmussen <kromann,joakim.rasmussen@live.dk>
2018-03-27 16:45:46 +0200

    Billede side virker ligsom tegne side

---
+++ Moccie/Billeder.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Billeder : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            MultiView1.ActiveViewIndex = 0;
+        }
+    }
+    protected void LinkButtonGrupper_Command(object sender, CommandEventArgs e)
+    {
+        MultiView1.ActiveViewIndex = 1;
+    }
+
+    protected void LinkButtonEnkeltProdukt_Command(object sender, CommandEventArgs e)
+    {
+        MultiView1.ActiveViewIndex = 2;
+    }
+}
