92e1091576a7e9a4add5e5efb8a94a6a707c56c4
thomas markussen <thomas-tanja@live.dk>
2018-04-04 12:31:35 +0200

    lavet admin default side, mangler footer

--- Moccie/Admin/DefaultAdmin.aspx.cs
+++ Moccie/Admin/DefaultAdmin.aspx.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -8,6 +11,57 @@ using System.Web.UI.WebControls;
 public partial class Admin_DefaultAdmin : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void Button_FrontPage_Click(object sender, EventArgs e)
+    {
+        SqlConnection conn = new SqlConnection();
+        conn.ConnectionString = ConfigurationManager.ConnectionStrings["MoccieDBConnectionString"].ToString();
+        SqlCommand cmd = new SqlCommand();
+        cmd.Connection = conn;
+
+        //cmd.CommandText = "INSERT INTO  [InfoText] (Overskrift, Text) VALUES (@Overskrift, @Text)";
+        cmd.CommandText = "UPDATE InfoText SET Overskrift = @Overskrift, Text = @Text WHERE Lokation = 'FrontPage'";
+
+
+        cmd.Parameters.Add("@Overskrift", SqlDbType.NVarChar).Value = TextBox_FrontOverskrift.Text;
+        cmd.Parameters.Add("@Text", SqlDbType.NVarChar).Value = TextBox_FrontText.Text;
+
+        conn.Open();
+
+        cmd.ExecuteNonQuery();
+
+        conn.Close();
+
+        Response.Redirect("../Admin/DefaultAdmin.aspx");
+    }
+
+    protected void Button_About_Click(object sender, EventArgs e)
+    {
+        SqlConnection conn = new SqlConnection();
+        conn.ConnectionString = ConfigurationManager.ConnectionStrings["MoccieDBConnectionString"].ToString();
+        SqlCommand cmd = new SqlCommand();
+        cmd.Connection = conn;
+
+        //cmd.CommandText = "INSERT INTO  [InfoText] (Overskrift, Text) VALUES (@Overskrift, @Text)";
+        cmd.CommandText = "UPDATE IndexText SET Overskrift = @Overskrift, Text = @Text";
+
+
+        cmd.Parameters.Add("@Overskrift", SqlDbType.NVarChar).Value = TextBox_AboutOverskrift.Text;
+        cmd.Parameters.Add("@Text", SqlDbType.NVarChar).Value = TextBox_AboutText.Text;
+
+        conn.Open();
+
+        cmd.ExecuteNonQuery();
+
+        conn.Close();
+
+        Response.Redirect("../Admin/DefaultAdmin.aspx");
+    }
+
+    protected void Button_Footer_Click(object sender, EventArgs e)
     {
 
     }
