9bc172611cf4db6686df1327d4fe5541bc14efcf
Joakim Timm Kromann-Rasmussen <kromann,joakim.rasmussen@live.dk>
2018-05-06 23:47:21 +0200

    Admin Tegninger desgin work

--- Moccie/Admin/TegningerAdmin.aspx.cs
+++ Moccie/Admin/TegningerAdmin.aspx.cs
@@ -7,8 +7,5 @@ using System.Web.UI.WebControls;
 
 public partial class Admin_TegningerAdmin : System.Web.UI.Page
 {
-    protected void Page_Load(object sender, EventArgs e)
-    {
-
-    }
+   
 }
