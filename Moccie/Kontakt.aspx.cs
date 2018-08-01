d9964fe8b3f56439ebc4afea6042e02346b4b855
thomas markussen <thomas-tanja@live.dk>
2018-08-01 15:24:00 +0200

    mail noget

--- Moccie/Kontakt.aspx.cs
+++ Moccie/Kontakt.aspx.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Data.SqlClient;
 using System.IO;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
@@ -47,34 +48,23 @@ public partial class Kontakt : System.Web.UI.Page
         TextBoxEmail2.Text = "";
         //Followers.Text = "";
         Page.DataBind();
-        
-
-
-
-
-
-        
-
-
-
-
 
         //try
         //{
         //    MailMessage mailMessage = new MailMessage();
-        //    mailMessage.To.Add("thomas-tanja@live.dk"); // To Email
+        //    mailMessage.To.Add("tomis@tomis.dk"); // To Email
         //    mailMessage.From = new MailAddress(TextBoxEmail.Text); // From Eamil
         //    mailMessage.Subject = DropDownListGrund.SelectedValue; // Email subject
         //    mailMessage.Body = "Send fra: " + TextBoxFornavn.Text + " " + TextBoxEfternavn.Text + "\n" + TextBoxSubject.Text; // Email body 
-        //    //SmtpClient smtpClient = new SmtpClient("smtp.unoeuro.com");
-        //    SmtpClient smtpClient = new SmtpClient();
-        //    smtpClient.Host = "localhost"; // Host
+        //    SmtpClient smtpClient = new SmtpClient("smtp.unoeuro.com");
+        //    //SmtpClient smtpClient = new SmtpClient();
+        //    //smtpClient.Host = "localhost"; // Host
         //    smtpClient.Send(mailMessage);
         //    LabelMsg.Text = "E-mail sent!";
         //}
         //catch (Exception ex)
         //{
-        //    LabelMsg.Text = "Could not send the e-mail - error: " + ex.Message;
+        //    LabelMsg.Text = "Could not send the e-mail - error: " + ex.StackTrace;
         //}
     }
 }
